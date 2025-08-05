67d7bfcd6e479e0160d3a349a25b5028048634e5
Sung Hoon Jung <sunghjung3@utexas.edu>
2025-08-05 12:34:31 -0500

    ga_opt analysis scripts added

---
+++ ase_vasp/ga_opt/Mg/plot_convex_hull.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.interpolate import make_interp_spline, interp1d
+from scipy.optimize import curve_fit
+import ase
+from ase.io import read, write
+
+plt.rcParams.update({'font.size': 18, 'font.family': 'Arial', 'figure.figsize':(9,6)})
+ion = 'Mg'
+data = pd.read_csv(f'{ion}_ga_energy.csv')
+
+fig, ax = plt.subplots()
+ax.scatter(data[f'{ion}_count']/32, data['Form Energy (per atom)'], color = 'grey')
+
+
+ax.set_ylabel('Formation energy (eV/atom)', fontweight="bold")
+ax.set_xlabel(f'x in {ion}$_x$'+'Fe$_2$'+'(CN)$_6$', fontweight="bold")
+ax.set_ylim([-0.015,0])
+ax.tick_params(axis='x', labelsize=16)  # Change x-axis tick label size
+ax.tick_params(axis='y', labelsize=16)  # Change y-axis tick label size
+
+
+plt.savefig(f'convexhull_{ion}.pdf')
+plt.show()
